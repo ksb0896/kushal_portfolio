17e29714a8824b68355610615c624365ed00c0df
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2026-04-08 22:10:12 +0530

    block app.js script 404 -fix

--- scripts/app.js
+++ scripts/app.js
@@ -3,24 +3,14 @@
  * Handles both root domains (localhost) and subfolder domains (GitHub Pages)
  */
 const registerServiceWorker = async () => {
-  if (!('serviceWorker' in navigator)) {
-    console.warn("Service Workers are not supported in this browser.");
-    return;
-  }
+  if (!('serviceWorker' in navigator))  return;
 
   try {
-    const pathArray = window.location.pathname.split('/');
-
-    const repoName = pathArray[1]; 
-
-    const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
     const swPath = isLocal ? './serviceWorker.js' : `/${repoName}/serviceWorker.js`;
 
     const registration = await navigator.serviceWorker.register(swPath);
     
-    console.log('Service Worker registered successfully');
-    console.log('Scope:', registration.scope);
-    
+    console.log('Service Worker registered successfully at:', registration.scope); 
   } catch (error) {
     console.error('Service Worker registration failed:', error);
   }
