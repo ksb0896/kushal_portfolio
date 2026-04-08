5aa08422cfb84e53264a663359644a574d1a0421
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2026-04-08 19:43:21 +0530

    app banner installation --added

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,12 @@
-// console.log("1. Script file loaded");
-
 if ('serviceWorker' in navigator) {
-//   console.log("2. Browser supports Service Workers");
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('./serviceWorker.js')
       .then(reg => console.log("Registration successful!", reg.scope))
       .catch(err => console.error("Registration failed:", err));
   });
 }
+
+// App installation
+window.addEventListener('appinstalled', () => {
+  console.log('App installed successfully!');
+});
